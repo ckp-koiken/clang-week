5c090743ad13f90bddaed6dfaef05cf94c669127
ckp-koiken <kentaro.koi0710@gmail.com>
2022-03-01 18:57:21 +0900

    add struct_lecture

--- advance/example/example.c
+++ advance/example/example.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
-// 関数ポインタを引数としてとる関数
-void funcp(int (*)(int), int n);
-// 関数ポインタに用いる関数
-int dbl(int);
-int hlf(int);
+/* 共用体の定義 */
+union data {
+  int n;  // 整数のデータ
+  double d; // 実数のデータ
+  char s[16]; // 文字列のデータ
+};
 
 int main() {
-  funcp(dbl, 8); // 引数として関数を渡す
-  funcp(hlf, 8); // 引数として関数を渡す
-  return 0;
-}
-
-void funcp(int (*f)(int), int n) {
-  printf("引数：%d 結果：%d\n", n, f(n));
-}
-
-// 2倍を返す関数
-int dbl(int n) {
-  return n * 2;
-}
-
-// 2倍を返す関数
-int hlf(int n) {
-  return n / 2;
+  union data d[3];
+  d[0].n = 1;
+  d[1].d = 3.14;
+  strcpy(d[2].s, "Hello");
+  printf("%d %f %s\n", d[0].n, d[1].d, d[2].s);
 }
