be41318dfc74a4ba039772f6be061f15af177966
ckp-koiken <kentaro.koi0710@gmail.com>
2022-03-04 17:25:13 +0900

    add listex6-1

---
+++ advance/lecture/day6/listex6-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+  FILE *file;  // ファイルポインタ（出力用）
+  file = fopen("./test/sample.txt", "w");  // ファイルを書き込み用に開く
+  if(file == NULL) {
+    printf("ファイルが開けません。\n");
+    exit(1);
+  }
+  //  ファイルにデータを書き込む
+  fprintf(file, "Hello World.\r\n");  // ファイルに書く
+  fprintf(file, "ABCDEF\r\n");
+  fclose(file);
+  printf("hello\n");
+  return 0;
+}
