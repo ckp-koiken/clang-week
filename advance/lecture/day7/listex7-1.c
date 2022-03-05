b0ae33990354e2dc91b627373cfcf632b598deb8
ckp-koiken <kentaro.koi0710@gmail.com>
2022-03-05 15:50:41 +0900

    add initDatabase

--- advance/lecture/day7/listex7-1.c
+++ advance/lecture/day7/listex7-1.c
@@ -43,6 +43,7 @@ int main() {
   int i;
   char names[][LENGTH] = {"山田太郎", "太田美智子", "大山次郎", "山口さやか"};
   int ids[] = {1, 2, 2, 3};
+  initDatabase();
   for(i = 0; i < 4; i++) {  // データの登録
     add(ids[i], names[i]);
     printf("登録：%d %s\n", ids[i], names[i]);
